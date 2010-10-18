b2b285dc750ff1c6ce111704b22bcfd050db4e76
Dusty Leary <dleary@gmail.com>
2010-10-17 22:27:15 -0700

    change Board.text to an array instead of a string.  no real speed increase

--- gorules.js
+++ gorules.js
@@ -1,11 +1,14 @@
 function Board(size) {
-    var dots = '..........................................................................................................................................................................................................................................................................................................................................................................';
-    while(dots.length < size*size) { dots += dots; }
-    this.text = dots.substr(0, size*size);
+    this.text = new Array(size*size);
+    for(var i=0; i<size*size; i++) {
+        this.text[i] = 0;
+    }
     this.size = size;
 }
 
-Board.prototype.BLACK = '#';
+Board.prototype.EMPTY = 0;
+Board.prototype.BLACK = 1;
+Board.prototype.WHITE = 2;
 
 Board.prototype.bv = function(x,y) {
     return this.text[y*this.size+x];
@@ -17,7 +20,7 @@ Board.prototype.gl = function(x,y, c,l,v) {
     if(-1 != v.indexOf(String.fromCharCode(x+97,y+97))) return;
     v.push(String.fromCharCode(x+97,y+97));
     var nc = this.bv(x,y);
-    if(nc == '.') l.push(String.fromCharCode(x+97,y+97));
+    if(nc == this.EMPTY) l.push(String.fromCharCode(x+97,y+97));
     else if(nc == c) {
         this.gl(x-1,y, c,l,v);
         this.gl(x+1,y, c,l,v);
@@ -30,7 +33,7 @@ Board.prototype.getLiberties = function(x,y) {
     if(x<0 || y<0) return [];
     if(x>=this.size || y>=this.size) return [];
     var c = this.bv(x,y);
-    if(c == '.') return [];
+    if(c == this.EMPTY) return [];
     var l = [];
     var v = [];
     this.gl(x-1,y, c,l,v);
@@ -46,9 +49,7 @@ Board.prototype.countLiberties = function(x,y) {
 
 Board.prototype.makeMove = function(color, x,y) {
     var index = y*this.size+x;
-    var t = this.text;
-    t = t.substr(0,index)+color+t.substr(index+1);
-    this.text = t;
+    this.text[index] = color;
 }
 
 Board.prototype.log = function() {
@@ -56,44 +57,3 @@ Board.prototype.log = function() {
         console.log(this.text.substr(i*this.size, this.size));
     }
 }
-
-/*
-function isValidMove(board, position, color) {
-    if(board[index] != '.') return false;
-    return true;
-}
-
-function makeMove(board, index, color) {
-    if(!isValidMove(board, index, color)) {
-        throw 'illegal move';
-    }
-    var newBoard = board.substring(0,index)+color+board.substring(index+1);
-    return newBoard;
-}
-
-function isEmptyPoint(board, index) {
-    if(index<1) return false;
-    if(index>=board.length) return false;
-    var c = board[index];
-    return c=='.' || c=='k';
-}
-
-function boardSize(board) {
-    switch(board.length) {
-        case 361: return 19;
-        case 169: return 13;
-        case 81: return 9;
-        case 25: return 5;
-    }
-    return Math.sqrt(board.length);
-}
-
-function isSuicide(board, index, color) {
-    return !(false
-        || isEmptyPoint(board, index-1)
-        || isEmptyPoint(board, index+1)
-        || isEmptyPoint(board, index-boardSize(board))
-        || isEmptyPoint(board, index+boardSize(board))
-        );
-}
-*/
