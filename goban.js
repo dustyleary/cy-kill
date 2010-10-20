e1d77ec8e447494b001d4aeb533808ab2d5b7b43
Dusty Leary <dleary@gmail.com>
2010-10-20 01:43:30 -0700

    got a UI up, and a 'play out' button to do 1 random playout...  now, I just need a factor of 40,000 speedup...

--- goban.js
+++ goban.js
@@ -1,29 +1,7 @@
 function Goban(args) {
     var self = this;
     this.el = $(args.el)[0] || elIsRequired;
-    $(this.el).append("<label>Board Size: <select class='boardSize'></select></label>");
-    $(this.el).append("<label>Default color: <select class='defaultColor'><option value='black'>Black</option><option value='white'>White</option></select></label>");
-    var s = $('select.defaultColor', this.el)[0];
-    $(s).change(function() {
-        self.setBlacksTurn(this.value == 'black');
-    });
-    $(this.el).append("<label>Komi: "+this.defaultKomi+"</label>");
-    $(this.el).append("<br>");
-    var s = $('select.boardSize', this.el)[0];
-    $([5,9,13,19]).each(function(i,v) {
-        var o = $('<option>'+v+'x'+v+'</option>')[0];
-        o.value = v;
-        $(s).append(o);
-    });
-    $(s).change(function(a,b,c) {
-        self.resetBoard(s.value);
-    });
     $(this.el).append("<div class='boardContainer'><div class='board'></div></div>");
-    $(this.el).append("<div class='boardState'>Copy & paste board state:<br><textarea class='boardText'></textarea></div>");
-    var ta = $('textarea.boardText', this.el)[0];
-    $(ta).bind('input', function() { self.onBoardTextChanged(); });
-
-    this.resetBoard(9);
 }
 
 Goban.prototype.dotPoints = {
@@ -31,17 +9,6 @@ Goban.prototype.dotPoints = {
     13:[3,6,9],
     19:[3,9,15],
 };
-Goban.prototype.defaultKomi = 6.5;
-
-Goban.prototype.setBlacksTurn = function(b) {
-    this.blacksTurn = b;
-    var s = $('select.defaultColor', this.el)[0];
-    if(b === false) {
-        s.value = 'white';
-    } else {
-        s.value = 'black';
-    }
-}
 
 Goban.prototype.resetBoard = function(size, boardState) {
     this.boardState = [];
@@ -58,17 +25,13 @@ Goban.prototype.resetBoard = function(size, boardState) {
             this.boardState.push(ps);
         }
     }
-    this.boardSize = size;
-    this.setBlacksTurn(true);
-    this.updateBoardText();
+    this.size = size;
     this.buildBoard();
     this.updateBoardDisplay();
 }
 
 Goban.prototype.buildBoard = function() {
     var self = this;
-    var s = $('select.boardSize', this.el)[0];
-    s.value = this.boardSize;
     var b = $('.board', this.el)[0];
     b.innerHTML = '';
     function bindClick(x,y,el) {
@@ -77,20 +40,20 @@ Goban.prototype.buildBoard = function() {
         });
     }
     this.boardPoints = [];
-    for(var y=0; y<this.boardSize; y++) {
+    for(var y=0; y<this.size; y++) {
         var r = $("<div class='row'/>")[0];
         b.appendChild(r);
-        for(var x=0; x<this.boardSize; x++) {
+        for(var x=0; x<this.size; x++) {
             var p = $("<div class='point'/>")[0];
             this.boardPoints.push(p);
             r.appendChild(p);
-            if(x<(this.boardSize-1)) {
+            if(x<(this.size-1)) {
                 r.appendChild($("<div class='hline'/>")[0]);
             }
-            var dp = this.dotPoints[this.boardSize];
+            var dp = this.dotPoints[this.size];
             if(dp) {
                 if(-1 != dp.indexOf(x) && -1 != dp.indexOf(y)) {
-                    if(this.boardSize != 13 || Math.abs(x-y)!=3) {
+                    if(this.size != 13 || Math.abs(x-y)!=3) {
                         $(p).append("<div class='dot'/>");
                         $(p).addClass('asdf');
                     }
@@ -101,10 +64,10 @@ Goban.prototype.buildBoard = function() {
             p.appendChild(pHitbox);
             bindClick(x,y,pHitbox);
         }
-        if(y<(this.boardSize-1)) {
+        if(y<(this.size-1)) {
             var vlines = $("<div class='vlines'/>")[0];
             r.appendChild(vlines);
-            for(var x=0; x<this.boardSize-1; x++) {
+            for(var x=0; x<this.size-1; x++) {
                 vlines.appendChild($("<div class='vline'/>")[0]);
             }
         }
@@ -119,53 +82,21 @@ Goban.prototype.pointClass = {
 };
 
 Goban.prototype.updateBoardDisplay = function() {
-    for(var i=0; i<this.boardSize*this.boardSize; i++) {
+    for(var i=0; i<this.size*this.size; i++) {
         var pclass = this.pointClass[this.boardState[i]];
         $(this.boardPoints[i]).attr('class', 'point '+pclass);
     }
 }
 
-Goban.prototype.updateBoardText = function() {
-    var bt = $('.boardText', this.el)[0];
-    bt.value = this.boardState.join('');
-}
-
 Goban.prototype.clickPoint = function(x,y) {
-    var ps = this.getPointState(x,y);
-    var newPs = '.';
-    if(false) {
-    } else if(ps == '.') {
-        newPs = this.blacksTurn?'#':'O';
-    } else if(ps == '#') {
-        newPs = 'O';
-    } else if(ps == 'O') {
-        newPs = 'k';
-    }
-    this.setPointState(x,y,newPs);
-    this.updateBoardText();
-    this.updateBoardDisplay();
+    $(this).trigger('pointClicked', {x:x, y:y});
 }
 
 Goban.prototype.getPointState = function(x,y,state) {
-    return this.boardState[y*this.boardSize+x];
+    return this.boardState[y*this.size+x];
 }
 
 Goban.prototype.setPointState = function(x,y,state) {
-    this.boardState[y*this.boardSize+x] = state;
-}
-
-Goban.prototype.onBoardTextChanged = function() {
-    var ta = $('textarea.boardText', this.el)[0];
-    var bt = ta.value;
-    if(false) {
-    } else if(bt.length >= 19*19) {
-        this.resetBoard(19, bt);
-    } else if(bt.length >= 13*13) {
-        this.resetBoard(13, bt);
-    } else if(bt.length >= 9*9) {
-        this.resetBoard(9, bt);
-    } else {
-        this.resetBoard(5, bt);
-    } 
+    this.boardState[y*this.size+x] = state;
 }
 
