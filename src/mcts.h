6c43b51907766a321c57aa9e2f4dd0c5d0217c57
Dusty Leary <dleary@gmail.com>
2011-05-15 09:01:41 -0700

    fix search to look for moves that benefit current player at each ply, instead of assuming white will move in black's favor :o

--- src/mcts.h
+++ src/mcts.h
@@ -1,10 +1,10 @@
 #pragma once
 
 struct Mcts {
-    const uint kPlayouts; //11
-    const uint kExpandThreshold; //5
-    const uint kStepSize; //100
-    const double kUctK; //1.0
+    const uint kPlayouts;
+    const uint kExpandThreshold;
+    const uint kStepSize;
+    const double kUctK;
 
     struct Node {
         uint64_t zobrist;
@@ -24,6 +24,7 @@ struct Mcts {
     BoardState player;
     float komi;
     Point curBestMove;
+    uint total_playouts;
 
     Node root;
     std::map<uint64_t, Node> all_children;
@@ -45,6 +46,7 @@ struct Mcts {
         , kStepSize(kStepSize)
         , kUctK(kUctK)
         , curBestMove(Point::pass())
+        , total_playouts(0)
     {
     }
 
@@ -91,18 +93,27 @@ struct Mcts {
             c,
             r
         );
-        int num_wins = (player == BoardState::WHITE()) ? r.white_wins : r.black_wins;
-        record_node_visit(n, kPlayouts, num_wins);
+        total_playouts += kPlayouts;
+        record_node_visit(n, kPlayouts, r.black_wins);
     }
 
     void initRoots() {
         expand_node(board, player, &root);
     }
 
-    double uctValue(Node* n) {
-        double winrate = double(n->num_wins) / double(n->num_visits);
-        double uct = winrate + kUctK * sqrt(log((double)n->parent->num_visits) / (5 * n->num_visits));
-        return uct;
+    double getWinRate(Node* n) {
+        return double(n->num_wins) / double(n->num_visits);
+    }
+    double getUctNumber(Node* n) {
+        return kUctK * sqrt(log((double)n->parent->num_visits) / (1 * n->num_visits));
+    }
+
+    double getMoveWeight(Node* n, BoardState playerColor) {
+        double winrate = getWinRate(n);
+        if(playerColor != BoardState::BLACK()) {
+            winrate = 1 - winrate;
+        }
+        return winrate + getUctNumber(n);
     }
 
     void doTrace(const Board& b, BoardState c, Node* n) {
@@ -117,7 +128,7 @@ struct Mcts {
         } else {
             //find the best child
             Node* bestChild = NULL;
-            double bestValue = -100;
+            double bestValue = -100000;
             Point bestPoint;
 
             PointSet moves;
@@ -127,12 +138,7 @@ struct Mcts {
                 Board subboard(b);
                 subboard.playMoveAssumeLegal(c, p);
                 Node* child = get_or_make_node(subboard, n);
-                if(bestChild == NULL) {
-                    bestChild = child;
-                    bestValue = uctValue(child);
-                    bestPoint = p;
-                }
-                double thisValue = uctValue(child);
+                double thisValue = getMoveWeight(child, c);
                 if(thisValue >= bestValue) {
                     bestChild = child;
                     bestValue = thisValue;
@@ -168,23 +174,18 @@ struct Mcts {
         uint bestCount = 0;
         std::string influence = "INFLUENCE";
         std::string label = "LABEL";
+        std::string text = strprintf("TEXT %d playouts\n", total_playouts);
         for(uint i=0; i<moves.size(); i++) {
             Point p = moves[i];
             Board subboard(board);
             subboard.playMoveAssumeLegal(player, p);
             Node* child = get_or_make_node(subboard, &root);
             uint count = child->num_visits;
-            double winrate = double(child->num_wins) / double(child->num_visits);
-            if(count > bestCount) {
+            double winrate = getWinRate(child);
+            if(winrate > bestRate) {
                 bestMove = p;
                 bestCount = count;
                 bestRate = winrate;
-            } else if(count == bestCount) {
-                if(winrate > bestRate) {
-                    bestMove = p;
-                    bestCount = count;
-                    bestRate = winrate;
-                }
             }
             label += strprintf(" %s %d",
                     p.toGtpVertex(board.getSize()).c_str(),
@@ -195,7 +196,7 @@ struct Mcts {
         }
         std::string cname = (player==BoardState::BLACK()) ? "B" : "W";
         std::string var = strprintf("VAR %s %s", cname.c_str(), bestMove.toGtpVertex(board.getSize()).c_str());
-        std::string gfx = "gogui-gfx:\n"+var+"\n"+influence+"\n"+label+"\n\n";
+        std::string gfx = "gogui-gfx:\n"+var+"\n"+influence+"\n"+label+"\n"+text+"\n\n";
         fputs(gfx.c_str(), stderr);
         curBestMove = bestMove;
     }
